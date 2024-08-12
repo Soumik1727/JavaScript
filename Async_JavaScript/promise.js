fa0d407d9bf06934dcc6368e4bb28ed515244571
Soumik Mondal <soumikmondal1727@gmail.com>
2024-08-12 11:15:27 +0530

    Promises

---
+++ Async_JavaScript/promise.js
@@ -0,0 +1,50 @@
+
+const getTodos = (resource ,callback) => {
+    
+    return new Promise( (resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange',() => {
+        
+        if( request.readyState === 4 && request.status === 200 ){
+                        // parse()=> it takes a JSON string and converts the string into a JS object.
+            const data = JSON.parse(request.responseText);
+            resolve(data);
+        }
+        else if(request.readyState === 4){
+            reject("Error getting resource!");
+        }
+    });
+
+    request.open('GET',resource);
+    request.send();
+    });
+};
+
+getTodos('./Async_JavaScript/json.json').then ( (data) => {
+    console.log('Resolved: ',data);
+}).catch( (err) => {
+    console.log('Error occured! ', err);
+});
+
+    // Promises  // Basic   // Syntax
+    // Promises  // Basic   // Syntax
+
+// const getSomething = () => {
+
+//     return new Promise( (resolve, reject) => {
+//         // Fetch something
+//         resolve('Get something...');
+//     });
+// }
+// getSomething().then( (data) => {         
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+// getSomething().then( data => {
+//     console.log(data);
+// }).catch( err => {
+//     console.log(err);
+// });
